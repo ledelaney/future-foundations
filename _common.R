c7f0ee6abdfde39e2d3225d4863b9e11cba30d4b
Lucy Delaney <ldelan5@uic.edu>
2021-06-01 19:32:35 -0500

    website bones

--- _common.R
+++ _common.R
@@ -1,3 +1,5 @@
+
+
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
@@ -6,6 +8,12 @@ knitr::opts_chunk$set(
   fig.align = 'center',
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold"
+  fig.show = "hold",
+  warning = FALSE,
+  eval = TRUE,
+  include = TRUE,
+  echo=FALSE, 
+  error = FALSE,
+  message = FALSE
 )
 
